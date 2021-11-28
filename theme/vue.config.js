f806b851b1018d32e6327717db8b03b88776a9d6
u2ki <u2ki.kp27@gmail.com>
2021-11-29 01:35:48 +0900

    add: Vuetifyのテンプレート追加

---
+++ theme/vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+  },
+  transpileDependencies: ['vuetify'],
+
+  // for Laravel
+  outputDir: '../public/app',
+  publicPath: '/app',
+  pages: {
+    app: {
+      entry: 'src/main.js',
+      template: 'templates/base.html',
+      filename: '../../resources/views/spa/app.blade.php',
+    },
+  }
+
