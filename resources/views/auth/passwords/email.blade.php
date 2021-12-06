bc9b054576b1c7a2af223d30adaa36e5cf90bf45
u2ki <u2ki.kp27@gmail.com>
2021-12-07 01:57:03 +0900

    fix: 他authまわりのレイアウト変更

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,44 @@
 @extends('layouts.v-app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <v-main class="py-5">
+        <v-container>
+            <v-row justify="center">
+                <v-col cols="12" lg="6" md="10" sm="10">
+                    <v-card elevation="0" tile>
+                        <div class="pa-8">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            <form method="POST" action="{{ route('password.email') }}" class="py-3">
+                                @csrf
+
+                                <v-text-field
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value="{{ old('email') }}"
+                                    v-bind:error="@error('email') true @else false @enderror"
+                                    error-messages="@error('email') {{$message}} @enderror"
+                                    required
+                                    autocomplete="email"
+                                    label={{ __('E-Mail Address') }}
+                                        placeholder="test@hoge.com"
+                                ></v-text-field>
+
+                                <div class="px-4 mb-4 mt-10">
+                                    <v-btn type="submit" color="primary" elevation="0" block large>
+                                        {{ __('Send Password Reset Link') }}
+                                    </v-btn>
+                                </div>
+                            </form>
                         </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,57 @@
 @extends('layouts.v-app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    <v-main class="py-5">
+        <v-container>
+            <v-row justify="center">
+                <v-col cols="12" lg="6" md="10" sm="10">
+                    <v-card elevation="0" tile>
+                        <div class="pa-8">
+                            <form method="POST" action="{{ route('password.update') }}" class="py-3">
+                                @csrf
+                                <input type="hidden" name="token" value="{{ $token }}">
+                                <v-text-field
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value="{{ $email ?? old('email') }}"
+                                    v-bind:error="@error('email') true @else false @enderror"
+                                    error-messages="@error('email') {{$message}} @enderror"
+                                    required
+                                    autocomplete="email"
+                                    label={{ __('E-Mail Address') }}
+                                        placeholder="test@hoge.com"
+                                ></v-text-field>
+                                <v-text-field
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    v-bind:error="@error('password') true @else false @enderror"
+                                    error-messages="@error('password') {{$message}} @enderror"
+                                    required
+                                    autocomplete="new-password"
+                                    label={{ __('Password') }}
+                                ></v-text-field>
+                                <v-text-field
+                                    type="password"
+                                    name="password_confirmation"
+                                    id="password-confirm"
+                                    required
+                                    autocomplete="new-password"
+                                    label={{ __('Confirm Password') }}
+                                ></v-text-field>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
 
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                                <div class="px-4 mb-4 mt-10">
+                                    <v-btn type="submit" color="primary" elevation="0" block large>
+                                        {{ __('Reset Password') }}
+                                    </v-btn>
+                                </div>
+                            </form>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
 @endsection
