28722c55a69dfdc5dc4d1a068f41d96ec9877dcc
u2ki <u2ki.kp27@gmail.com>
2021-12-07 01:09:50 +0900

    fix: login/register レイアウト変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,20 +2,15 @@
 @extends('layouts.v-app')
 
 @section('content')
-    <v-main>
+    <v-main class="py-5">
         <v-container>
-            <h1 class="h4 text-center pt-8 font-weight-bold">
-                {{ __('Login') }}
-            </h1>
             <v-row justify="center">
-                <v-col cols="12" md="6">
-                    <v-card elevation="0">
+                <v-col cols="12" lg="6" md="10" sm="10">
+                    <v-card elevation="0" tile>
                         <div class="pa-8">
-                            <form method="POST" action="{{ route('login') }}">
+                            <form method="POST" action="{{ route('login') }}" class="py-3">
                                 @csrf
-                                <p class="mb-0 font-weight-bold">{{ __('E-Mail Address') }}</p>
                                 <v-text-field
-                                    outlined
                                     type="email"
                                     name="email"
                                     value="{{ old('email') }}"
@@ -23,26 +18,27 @@
                                     error-messages="@error('email') {{$message}} @enderror"
                                     required
                                     autocomplete="email"
-                                    placeholder="example@example.com"
+                                    label={{ __('E-Mail Address') }}
+                                    placeholder="test@hoge.com"
                                     autofocus
+                                    clearable
                                 ></v-text-field>
-
-                                <p class="mb-0 font-weight-bold">{{ __('Password') }}</p>
                                 <v-text-field
-                                    outlined
                                     type="password"
                                     name="password"
                                     v-bind:error="@error('password') true @else false @enderror"
                                     error-messages="@error('password') {{$message}} @enderror"
                                     required
                                     autocomplete="current-password"
+                                    label={{ __('Password') }}
+                                    clearable
                                 ></v-text-field>
 
                                 <v-checkbox
                                     name="remember"
                                     label="{{ __('Remember Me') }}"
                                     value="{{ old('remember') ? 'true' : 'false' }}"
-                                    class="mb-12"
+                                    class="mt-4 mb-4"
                                 ></v-checkbox>
 
                                 <div class="px-4 mb-4">
@@ -59,6 +55,37 @@
                                     </div>
                                 @endif
                             </form>
+                            <div class="separator separator_login_page">
+                                <div class="middle_separator">または</div>
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <v-btn
+                                    class="fill-width text-capitalize caption mt-3 mb-16"
+                                    height="48px"
+                                    outlined
+                                    style="border-color:#979797;"
+                                    tile
+                                >
+                                    <img
+                                        class="button-logo-img mr-4"
+                                        src="https://madeby.google.com/static/images/google_g_logo.svg"
+                                        style="height:24px;"
+                                    />
+                                    Googleアカウントでログイン
+                                </v-btn>
+                            </div>
+                            <div class="">
+                                <v-btn
+                                    class="fill-width overline auth-to-btn"
+                                    outlined
+                                    style="border-color:#979797;"
+                                    tile
+                                    href="{{ route('register') }}"
+                                >
+                                    <v-icon class="pe-5">mdi-chevron-right</v-icon>
+                                    <span class="auth-text">新規登録</span>
+                                </v-btn>
+                            </div>
                         </div>
                     </v-card>
                 </v-col>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,97 @@
 @extends('layouts.v-app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <v-main class="py-5">
+        <v-container>
+            <v-row justify="center">
+                <v-col cols="12" lg="6" md="10" sm="10">
+                    <v-card elevation="0" tile>
+                        <div class="pa-8">
+                            <form method="POST" action="{{ route('register') }}" class="py-3">
+                                @csrf
+                                <v-text-field
+                                    type="text"
+                                    name="name"
+                                    value="{{ old('name') }}"
+                                    v-bind:error="@error('name') true @else false @enderror"
+                                    error-messages="@error('name') {{$message}} @enderror"
+                                    required
+                                    autocomplete="name"
+                                    label={{ __('Name') }}
+                                    autofocus
+                                ></v-text-field>
+                                <v-text-field
+                                    type="email"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    v-bind:error="@error('email') true @else false @enderror"
+                                    error-messages="@error('email') {{$message}} @enderror"
+                                    required
+                                    autocomplete="email"
+                                    label={{ __('E-Mail Address') }}
+                                    placeholder="test@hoge.com"
+                                ></v-text-field>
+                                <v-text-field
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    v-bind:error="@error('password') true @else false @enderror"
+                                    error-messages="@error('password') {{$message}} @enderror"
+                                    required
+                                    autocomplete="new-password"
+                                    label={{ __('Password') }}
+                                ></v-text-field>
+                                <v-text-field
+                                    type="password"
+                                    name="password_confirmation"
+                                    id="password-confirm"
+                                    required
+                                    autocomplete="new-password"
+                                    label={{ __('Confirm Password') }}
+                                ></v-text-field>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                                <div class="px-4 mb-4">
+                                    <v-btn type="submit" color="primary" elevation="0" block large>
+                                        {{ __('Register') }}
+                                    </v-btn>
+                                </div>
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                            </form>
+                            <div class="separator separator_login_page">
+                                <div class="middle_separator">または</div>
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <v-btn
+                                    class="fill-width text-capitalize caption mt-3 mb-16"
+                                    height="48px"
+                                    outlined
+                                    style="border-color:#979797;"
+                                    tile
+                                >
+                                    <img
+                                        class="button-logo-img mr-4"
+                                        src="https://madeby.google.com/static/images/google_g_logo.svg"
+                                        style="height:24px;"
+                                    />
+                                    Googleアカウントで新規登録
+                                </v-btn>
+                            </div>
+                            <div class="">
+                                <v-btn
+                                    class="fill-width overline auth-to-btn"
+                                    outlined
+                                    style="border-color:#979797;"
+                                    tile
+                                    href="{{ route('login') }}"
+                                >
+                                    <v-icon class="pe-5">mdi-chevron-right</v-icon>
+                                    <span class="auth-text">ログイン</span>
+                                </v-btn>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-main>
 @endsection
