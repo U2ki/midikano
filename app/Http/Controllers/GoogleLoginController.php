a4cabc0d5c13c7e07152aa5e530b439b339d080a
u2ki <u2ki.kp27@gmail.com>
2022-01-26 23:08:26 +0900

    heroku

---
+++ app/Http/Controllers/GoogleLoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleLoginController extends Controller
+{
+    public function getGoogleAuth()
+    {
+        return Socialite::driver('google')
+                        ->redirect();
+    }
+
+    public function authGoogleCallback()
+    {
+        $gUser = Socialite::driver('google')->stateless()->user();
+        // email が合致するユーザーを取得
+        $user = User::where('email', $gUser->email)->first();
+        // 見つからなければ新しくユーザーを作成
+        if ($user == null) {
+            $user = $this->createUserByGoogle($gUser);
+        }
+        // ログイン処理
+        Auth::login($user, true);
+        return redirect('/');
+    }
+
+    public function createUserByGoogle($gUser)
+    {
+        $user = User::create([
+            'name'     => $gUser->name,
+            'email'    => $gUser->email,
+            'password' => Hash::make(uniqid()),
+        ]);
+        return $user;
+    }
+}
