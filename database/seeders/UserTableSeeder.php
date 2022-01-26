0572b28ea5f9c7d34c9a26d8a903e1a6047780bb
u2ki <u2ki.kp27@gmail.com>
2022-01-27 04:20:32 +0900

    heroku: テストログイン用seeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class UserTableSeeder extends Seeder
@@ -14,11 +15,33 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'test(管理者)',
-            'email' => 'test@test.com',
-            'password' => bcrypt('password'),
-            'status' => 2,
-        ]);
+        $params =
+            [
+                [
+                    'name' => 'test(管理者)',
+                    'email' => 'admin@admin.com',
+                    'password' => bcrypt('manamana'),
+                    'status' => 2,
+                ],
+                [
+                    'name' => 'test(関係者)',
+                    'email' => 'hoge@hoge.com',
+                    'password' => bcrypt('hogehoge'),
+                    'status' => 1,
+                ],
+                [
+                    'name' => 'test(一般)',
+                    'email' => 'test@test.com',
+                    'password' => bcrypt('manamana'),
+                    'status' => 0,
+                ]
+            ];
+
+        $now = Carbon::now();
+        foreach ($params as $param) {
+            $param['created_at'] = $now;
+            $param['updated_at'] = $now;
+            DB::table('users')->insert($param);
+        }
     }
 }
